f01a4af9263327fdb6b3a3236ab97b078767fbe5
botofeight <141572155+botofeight@users.noreply.github.com>
2024-04-18 20:44:30 -0400

    removed newlines from claude labels and program to download images based on this labeling

---
+++ data/autoLabelModels/move_images_to_dataset.py
@@ -0,0 +1,13 @@
+from claude_autoLabel import move_images_to_dataset
+
+image_link_file_path = "../imageScraping/LLMBrowser/image_urls/outputaa"
+label_file_path = "label_file_processed.txt"
+dataset_path = "testdataset"
+acceptable_labels = ['0','1','2','3','4','5', 'N/A']
+
+move_images_to_dataset(image_link_file_path, label_file_path, dataset_path, acceptable_labels=acceptable_labels)
+
+
+image_link_file_path = "../imageScraping/LLMBrowser/image_urls/outputab"
+label_file_path = "label_file2_processed.txt"
+move_images_to_dataset(image_link_file_path, label_file_path, dataset_path, acceptable_labels=acceptable_labels)
