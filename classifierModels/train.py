33fe13c70e10b1ac8f4b02e7809cbb3dc967e9f3
UjirYed <rd3054@columbia.edu>
2024-04-19 23:56:07 -0400

    train.py modifications
    added functions and easier to train models with specific hyperparameters.

--- classifierModels/train.py
+++ classifierModels/train.py
@@ -40,11 +40,67 @@ def compute_metrics(p):
 
 def no_grad(model):
     for p in model.parameters():
-        p.requires_grad= False
+        p.requires_grad = False
 
 def yes_grad(model):
     for p in model.parameters():
-        p.requires_grad= True
+        p.requires_grad = True
+
+def train(model, save_dir, batch_size, num_epochs, collate_fn, compute_metrics, trainOnlyHead):
+
+    if trainOnlyHead == True:
+        no_grad(model)
+        yes_grad(model.classifier)
+
+    training_args = TrainingArguments(
+        output_dir = save_dir,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=32,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        num_train_epochs=num_epochs,
+        lr_scheduler_type="cosine",
+        logging_steps=10,
+        save_total_limit=2,
+        remove_unused_columns=False,
+        push_to_hub=False,
+        load_best_model_at_end=True,
+        dataloader_num_workers=0,
+        gradient_accumulation_steps=8,
+    )
+
+    ## setting base learning rate
+    base_learning_rate = 1e-3
+    total_train_batch_size = (
+        training_args.train_batch_size * training_args.gradient_accumulation_steps * training_args.world_size
+    )
+
+    training_args.learning_rate = base_learning_rate * total_train_batch_size / 256
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=image_datasets['train'],
+        eval_dataset=image_datasets['val'],
+        #tokenizer=image_processor,
+        compute_metrics=compute_metrics,
+        data_collator=collate_fn
+    )
+
+    train_results = trainer.train()
+
+    return train_results
+
+
+def ImageFolderDataSets(data_dir, data_transforms):
+    """
+    returns dictionary of train and val datasets
+    """
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                            data_transforms[x])
+                    for x in ['train', 'val']}
+    return image_datasets
+
 
 if __name__ == "__main__":
     # Create the data transforms
@@ -63,22 +119,37 @@ if __name__ == "__main__":
         ]),
     }
 
-    # Load the dataset
-    data_dir = "../data/dataset/"
-    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                            data_transforms[x])
-                    for x in ['train', 'val']}
+    # loading training and validation datasets
+
+    image_datasets = ImageFolderDataSets("../data/dataset/", data_transforms)
     
+    
+    ## RESNET TRAINER
+
     resnet_custom_config = ResNetConfig(
         embedding_size = 64,
         hidden_sizes = [256, 512, 1024, 2048],
-        depths = [3, 4, 6, 3],
+        depths = [3, 4, 6, 3, ],
         layer_type = "bottleneck",
         hidden_act = "relu",
         out_features = ["stage1"],
         num_labels = 5,
         #num_hidden_layers = 3,
     )
+
+    resnet_finetune = {
+        "model": ResNetForImageClassification(resnet_custom_config),
+        "save_dir": "pretrained_resnet",
+        "batch_size": 32,
+        "num_epochs":  50,
+        "collate_fn": collate_fn,
+        "compute_metrics": compute_metrics,
+        "trainOnlyHead": True
+    }
+
+    train(**resnet_finetune)
+
+    ##EFFICENT_NET TRAINER
 
     efficientNet_custom_config = EfficientNetConfig(
         embedding_size = 64,
@@ -93,62 +164,4 @@ if __name__ == "__main__":
         #num_hidden_layers = 3,
     )
 
-    # Instantiate each model into a dictionary with the model and its corresponding save directory
-    resnet_from_scratch = {"model": ResNetForImageClassification(resnet_custom_config),
-                           "save_dir": "./resnet_from_scratch"}
-
-    resnet_pretrained = {"model": ResNetForImageClassification.from_pretrained("microsoft/resnet-50", num_labels = 5, ignore_mismatched_sizes=True),
-                         "save_dir": "./resnet_pretrained"}
-    no_grad(resnet_pretrained["model"])
-    yes_grad(resnet_pretrained["model"].classifier)
-
-    efficientnet_from_scratch = {"model": EfficientNetForImageClassification(efficientNet_custom_config),
-                                "save_dir": "./efficientnet_from_scratch"}
-    
-    efficientnet_pretrained = {"model": EfficientNetForImageClassification.from_pretrained("google/efficientnet-b0", num_labels = 5, ignore_mismatched_sizes=True),
-                               "save_dir": "./efficientnet_pretrained"}
-    no_grad(efficientnet_pretrained["model"])
-    yes_grad(efficientnet_pretrained["model"].classifier)
-
-    # Create the training args
-    training_args = TrainingArguments(
-        output_dir = resnet_pretrained["save_dir"],
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        num_train_epochs=100,
-        lr_scheduler_type="cosine",
-        logging_steps=10,
-        save_total_limit=2,
-        remove_unused_columns=False,
-        push_to_hub=False,
-        load_best_model_at_end=True,
-        dataloader_num_workers=0,
-        #gradient_accumulation_steps=8,
-    )
-
-    # Compute the learning rate from the base learning rate
-    base_learning_rate = 1e-3
-    total_train_batch_size = (
-        training_args.train_batch_size * training_args.gradient_accumulation_steps * training_args.world_size
-    )
-
-    training_args.learning_rate = base_learning_rate * total_train_batch_size / 256
-
-    # Create the trainer
-    trainer = Trainer(
-        model=resnet_pretrained["model"],
-        args=training_args,
-        train_dataset=image_datasets['train'],
-        eval_dataset=image_datasets['val'],
-        #tokenizer=image_processor,
-        compute_metrics=compute_metrics,
-        data_collator=collate_fn
-    )
-
     print(torch.cuda.is_available())
-
-    train_results = trainer.train()
-
-    print(train_results)
